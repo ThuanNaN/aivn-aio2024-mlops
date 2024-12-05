fc7053b63028c76fce480eef14d32e9858128113
ThuanNaN <duongthuan1445@gmail.com>
2024-12-05 05:58:38 +0000

    fix: wrong target dir

--- Week-05-AirFlow/platform/dags/deploy.sh
+++ Week-05-AirFlow/platform/dags/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="."
+AIRFLOW_DIR="../airflow/"
 DAGS_DIR="../airflow/dags/"
 CONFIG_DIR="../airflow/config/"
 
@@ -11,4 +12,4 @@ rsync -av --progress --include='*.py' --exclude='*' "$SOURCE_DIR" "$DAGS_DIR"
 rsync -av --progress --include='*.yaml' --exclude='*' "$SOURCE_DIR" "$CONFIG_DIR"
 
 # Sync requirements.txt to config folder
-rsync -av --progress --include='requirements.txt' --exclude='*' "$SOURCE_DIR" "$CONFIG_DIR"
+rsync -av --progress --include='requirements.txt' --exclude='*' "$SOURCE_DIR" "$AIRFLOW_DIR"
