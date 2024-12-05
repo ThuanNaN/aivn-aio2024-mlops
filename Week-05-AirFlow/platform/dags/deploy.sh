8fa69899016950828d17aca6553dae09155e162e
ThuanNaN <duongthuan1445@gmail.com>
2024-12-05 05:18:00 +0000

    add: deploy requirements.txt

--- Week-05-AirFlow/platform/dags/deploy.sh
+++ Week-05-AirFlow/platform/dags/deploy.sh
@@ -9,3 +9,6 @@ rsync -av --progress --include='*.py' --exclude='*' "$SOURCE_DIR" "$DAGS_DIR"
 
 # Sync .yaml files to config folder
 rsync -av --progress --include='*.yaml' --exclude='*' "$SOURCE_DIR" "$CONFIG_DIR"
+
+# Sync requirements.txt to config folder
+rsync -av --progress --include='requirements.txt' --exclude='*' "$SOURCE_DIR" "$CONFIG_DIR"
