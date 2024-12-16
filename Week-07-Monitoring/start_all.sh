6cd513428f1a11cdd07ada4053e06f6f39a6aafb
ThuanNaN <duongthuan1445@gmail.com>
2024-12-17 06:05:25 +0700

    fix: absolute path

--- Week-07-Monitoring/start_all.sh
+++ Week-07-Monitoring/start_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /mnt/nvme/Repository/aivn-aio2024-mlops/Week-07-Monitoring/platform/mlflow
+cd ./platform/mlflow
 docker compose up -d --build
 
-cd /mnt/nvme/Repository/aivn-aio2024-mlops/Week-07-Monitoring/platform/airflow
+cd ./platform/airflow
 docker compose up -d --build
 
-cd /mnt/nvme/Repository/aivn-aio2024-mlops/Week-07-Monitoring/backend 
+cd ./backend 
 docker compose up -d --build
 
-cd /mnt/nvme/Repository/aivn-aio2024-mlops/Week-07-Monitoring/frontend
+cd ./frontend
 docker compose up -d --build
